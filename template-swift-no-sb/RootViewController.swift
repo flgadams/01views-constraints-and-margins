e6c62115dcb5d01f7976380d2dcf18f4adbd3730
Glenn Adams <flgadams@gmail.com>
2022-08-06 20:18:46 -0400

    remove middle then add back

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -37,37 +37,36 @@ class RootViewController: UIViewController {
         self.view.addSubview(v3)
         
         // 3 horizontal stripes v1,v2,v3
-        let c1 = NSLayoutConstraint.constraints(withVisualFormat:
+        let topStripeHor = NSLayoutConstraint.constraints(withVisualFormat:
             "H:|-(20)-[v(100)]", metrics: nil, views: ["v":v1])
-        let c2 = NSLayoutConstraint.constraints(withVisualFormat:
+        let middleStripeHor = NSLayoutConstraint.constraints(withVisualFormat:
             "H:|-(20)-[v(100)]", metrics: nil, views: ["v":v2])
-        let c3 = NSLayoutConstraint.constraints(withVisualFormat:
+        let bottomStripeHor = NSLayoutConstraint.constraints(withVisualFormat:
             "H:|-(20)-[v(100)]", metrics: nil, views: ["v":v3])
 
         // first stripe 100 from top
-        let c4 = NSLayoutConstraint.constraints(withVisualFormat:
-            "V:|-(100)-[v(20)]", metrics: nil, views: ["v":v1])
+        let topStripeVert = NSLayoutConstraint.constraints(withVisualFormat:
+            "V:|-(600)-[v(20)]", metrics: nil, views: ["v":v1])
         // vertial with all 3 : 2,3,4, below v1
-        let c5with = NSLayoutConstraint.constraints(withVisualFormat:
+        let middleAndBotVert = NSLayoutConstraint.constraints(withVisualFormat:
             "V:[v1]-(20)-[v2(20)]-(20)-[v3(20)]", metrics: nil,
             views: ["v1":v1, "v2":v2, "v3":v3])
         // vertical for top and bottom only
-        let c5without = NSLayoutConstraint.constraints(withVisualFormat:
+        let bottomVert = NSLayoutConstraint.constraints(withVisualFormat:
             "V:[v1]-(20)-[v3(20)]", metrics: nil, views: ["v1":v1, "v3":v3])
 
-        NSLayoutConstraint.activate([c1,c3,c4].flatMap{$0})
+        NSLayoutConstraint.activate([topStripeHor,bottomStripeHor,topStripeVert].flatMap{$0})
         
         // top and bottom horizontal stripes
-        self.constraintsWith.append(contentsOf: c2) // middle bar horiz
+        self.constraintsWith.append(contentsOf: middleStripeHor) // middle bar horiz
         // vertical 1,2, and 3
-        self.constraintsWith.append(contentsOf: c5with) // 3 horiz stripes vertially
+        self.constraintsWith.append(contentsOf: middleAndBotVert) // 3 horiz stripes vertially
         // activate 3 stripes
         NSLayoutConstraint.activate(self.constraintsWith)
         // vertical for 1 and 3 only
-        self.constraintsWithout.append(contentsOf: c5without)
+        self.constraintsWithout.append(contentsOf: bottomVert)
         
         Task {
-            
             try await Task.sleep(nanoseconds: 5000000000)
             print("remove middle constraints ")
             //MUST REVMOVE MIDDLE VIEW FROM SUPERVIEW
@@ -76,6 +75,14 @@ class RootViewController: UIViewController {
             NSLayoutConstraint.deactivate(self.constraintsWith)
             NSLayoutConstraint.activate(self.constraintsWithout)
 
+        }
+        
+        Task {
+            
+            try await Task.sleep(nanoseconds: 10000000000)
+            self.view.addSubview(v2 ) // add the middle stripe
+            NSLayoutConstraint.deactivate(constraintsWithout)
+            NSLayoutConstraint.activate(constraintsWith)
             
         }
     }
